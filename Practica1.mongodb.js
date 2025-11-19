5bd7ce682be6f744cf110622ffd24d7b0c413b38
amayarodriguezana46-eng <amaya.rodriguez.ana.46@cbtis118.edu.mx>
2025-11-19 18:31:01 +0000

    Se agrega colecciones a Practica1

---
+++ Practica1.mongodb.js
@@ -0,0 +1,6 @@
+use('Colecciones');
+db.createCollection('Artistas');
+db.createCollection('Musica');
+db.createCollection('Cantantes');
+db.createCollection('Obras');
+db.createCollection('Deportes');
