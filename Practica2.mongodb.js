605715325f0ced707d0978c910fa58d5e502e850
amayarodriguezana46-eng <amaya.rodriguez.ana.46@cbtis118.edu.mx>
2025-11-19 19:13:52 +0000

    Se agregaron datos

--- Practica2.mongodb.js
+++ Practica2.mongodb.js
@@ -26,3 +26,42 @@ db.Cantantes.insertMany([
   {nombre: "Jimin", edad: 30, grupo: "BTS" },
   {nombre: "Taehyung", edad: 30, grupo: "BTS" },
   ]);  
+
+db.Deportes.insertMany([
+  {nombre: "Fútbol", tipo: 17, lugar: "Estadio" },
+  {nombre: "Baloncesto", tipo: 17, lugar: "Cancha de baloncesto" },
+  {nombre: "Rugby", tipo: 17, lugar: "Campo de rugby" },
+  {nombre: "Voleibol", tipo: 17, lugar: "Cancha de voleibol" },
+  {nombre: "Boxeo", tipo: 17, lugar: "Ring de boxeo" },
+  {nombre: "Fútbol Americano", tipo: 17, lugar: "Campo de fútbol americano" },
+  {nombre: "Tenis", tipo: 17, lugar: "Cancha de tenis" },
+  {nombre: "MMA", tipo: 17, lugar: "Octágono" },
+  {nombre: "Cricket", tipo: 17, lugar: "Campo de cricket" },
+  {nombre: "Golf", tipo: 17, lugar: "Campo de golf" }
+]); 
+
+db.Musica.insertMany([
+  {genero: "Pop", integrantes: 5},
+  {genero: "Rock", integrantes: 4},
+  {genero: "Jazz", integrantes: 4},
+  {genero: "Hip-Hop", integrantes: 4},
+  {genero: "Reguetón", integrantes: 4},
+  {genero: "Electrónica", integrantes: 4},
+  {genero: "R&B", integrantes: 4},
+  {genero: "Indie", integrantes: 4},
+  {genero: "K-pop", integrantes: 4},
+  {genero: "Country", integrantes: 4}
+]);
+
+db.Obras.insertMany([
+  {nombre: "Hamlet", genero: "Tragedia", creador: "William Shakespeare"},
+  {nombre: "La Casa de Bernarda Alba", genero: "Drama", creador: "Federico García Lorca"},
+  {nombre: "Don Juan Tenorio", genero: "Comedia", creador: "Tirso de Molina"},
+  {nombre: "Macbeth", genero: "Tragedia", creador: "William Shakespeare"},
+  {nombre: "Esperando a Godot", genero: "Absurdo", creador: "Samuel Beckett"},
+  {nombre: "El Cid", genero: "Épico", creador: "Anónimo"},
+  {nombre: "Romeo y Julieta", genero: "Tragedia", creador: "William Shakespeare"},
+  {nombre: "Un tranvía llamado Deseo", genero: "Drama", creador: "Tennessee Williams"},
+  {nombre: "Las aventuras de Tom Sawyer", genero: "Comedia", creador: "Mark Twain"},
+  {nombre: "La ópera de tres centavos", genero: "Teatro musical", creador: "Bertolt Brecht"}
+]);
