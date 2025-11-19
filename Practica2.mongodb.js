420b9a8875311d271e620a5875d44ae3a9542763
amayarodriguezana46-eng <amaya.rodriguez.ana.46@cbtis118.edu.mx>
2025-11-19 18:52:35 +0000

    Se agregaron datos a artistas

---
+++ Practica2.mongodb.js
@@ -0,0 +1,14 @@
+use('Colecciones');
+db.Artistas.insertMany([
+    {nombre: "Diego", edad: 17, grupo: "Wiplash" },
+    {nombre: "Jota", edad: 22, grupo: "Wiplash" },
+    {nombre: "Daan", edad: 21, grupo: "Wiplash" },
+    {nombre: "Smoke", edad: 22, grupo: "Wiplash" },
+    {nombre: "Jorge", edad: 19, grupo: "Solista" },
+    {nombre: "German", edad: 22, grupo: "DAAZ" },
+    {nombre: "Harry", edad: 31, grupo: "One Direction" },
+    {nombre: "Liam", edad: 32, grupo: "One Direction" },
+    {nombre: "Jungkook", edad: 28, grupo: "BTS" },
+    {nombre: "Jimin", edad: 30, grupo: "BTS" },
+    {nombre: "Taehyung", edad: 30, grupo: "BTS" },
+    ]);
