401c84ba0ee1c409acb804361d26f6fe6dd255aa
amayarodriguezana46-eng <amaya.rodriguez.ana.46@cbtis118.edu.mx>
2025-11-26 17:39:48 +0000

    Agregue datos

--- Practica5.mongodb.js
+++ Practica5.mongodb.js
@@ -11,3 +11,14 @@
  db.Artistas.find({ obra: {$gt:"La persistencia de la memoria" }});
  db.Artistas.find({ nombre: {$gt:"Claude Monet" }});
  db.Artistas.find({ obra: {$gt:"Impresión, sol naciente" }});
+ db.Artistas.find({ nombre: {$gt:"Andy Warhol" }});
+ db.Artistas.find({ obra: {$gt:"Campbell's Soup Cans" }});
+ db.Artistas.find({ nombre: {$gt:"Leonardo Da Vinci" }});
+ db.Artistas.find({ obra: {$gt:"La Mona Lisa" }});
+ db.Artistas.find({ nombre: {$gt:"Michelangelo Buonarroti" }});
+ db.Artistas.find({ obra: {$gt:"La creación de Adán" }});
+ db.Artistas.find({ nombre: {$gt:"Henri Matisse" }});
+ db.Artistas.find({ obra: {$gt:"La danza" }});
+ db.Artistas.find({ nombre: {$gt:"Georgia O'Keeffe" }});
+ db.Artistas.find({ obra: {$gt:"Flores rojas" }});
+ 
