8684e76a7eb06a3d2afe6b240a7102784885b168
amayarodriguezana46-eng <amaya.rodriguez.ana.46@cbtis118.edu.mx>
2025-11-26 18:41:30 +0000

    Cambie signos

---
+++ Resultado5.js
@@ -0,0 +1,11 @@
+
+[
+  {
+    "_id": {
+      "$oid": "692087879a68884cb3818911"
+    },
+    "nombre": "El Retrato de Dorian Gray",
+    "genero": "Filosófico",
+    "creador": "Oscar Wilde"
+  }
+]
